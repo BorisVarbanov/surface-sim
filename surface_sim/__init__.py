1ec2a4ec0f03f36a3c9de2ca8b87ce3307538ad4
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-07 02:00:46 +0200

    Update __init__.py

--- surface_sim/__init__.py
+++ surface_sim/__init__.py
@@ -1,18 +1,18 @@
 """Main surface-sim module."""
 __version__ = "0.1.0"
 
-from . import layouts, schedules
-from .circuits import Circuit, get_circuit
+from . import circuits, experiments, models
+from .circuits import Circuit
 from .layouts import Layout
-from .schedules import Schedule
+from .models import Model
 from .setup import Setup
 
 __all__ = [
-    "schedules",
-    "layouts",
-    "Schedule",
-    "get_circuit",
     "Layout",
-    "Setup",
     "Circuit",
+    "Setup",
+    "Model",
+    "models",
+    "circuits",
+    "experiments",
 ]
