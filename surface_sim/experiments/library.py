a0a19146d33b417f3ad7537416bee50dc912702e
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-10 19:48:50 +0100

    Update library.py
    Update experiment lib with new circuits.

--- surface_sim/experiments/library.py
+++ surface_sim/experiments/library.py
@@ -1,22 +1,20 @@
 from stim import Circuit
 
 from ..circuits import log_init, log_meas, qec_round
-from ..layouts import Layout
 from ..models import Model
 
 
 def memory_exp(
     model: Model,
-    layout: Layout,
     num_rounds: int,
     log_state: int,
     rot_basis: bool = False,
 ) -> Circuit:
 
-    init_circ = log_init(model, layout, log_state, rot_basis)
+    init_circ = log_init(model, log_state, rot_basis)
 
-    qec_circ = qec_round(model, layout)
-    meas_circuit = log_meas(model, layout, rot_basis)
+    qec_circ = qec_round(model)
+    meas_circuit = log_meas(model, rot_basis)
 
     experiment = init_circ + (qec_circ * num_rounds) + meas_circuit
     return experiment
