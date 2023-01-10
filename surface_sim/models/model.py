9c3f1d8a13a3fa6ec9ac0079bf1fa56858b6b72a
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-10 19:47:24 +0100

    Update model.py
    Add layout to model

--- surface_sim/models/model.py
+++ surface_sim/models/model.py
@@ -1,15 +1,21 @@
-from typing import Any, Iterable
+from typing import Any
 
+from ..layouts import Layout
 from ..setup import Setup
 
 
 class Model(object):
-    def __init__(self, setup: Setup) -> None:
+    def __init__(self, setup: Setup, layout: Layout) -> None:
         self._setup = setup
+        self._layout = layout
 
     @property
     def setup(self) -> Setup:
         return self._setup
 
-    def param(self, *qubits: Iterable[str]) -> Any:
+    @property
+    def layout(self) -> Layout:
+        return self._layout
+
+    def param(self, *qubits: str) -> Any:
         return self._setup.param(*qubits)
