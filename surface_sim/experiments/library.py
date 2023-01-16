21d89bb2ccd1ca82a89d374206f98049053e0b11
Boris Varbanov <b.m.varbanov@gmail.com>
2023-01-16 10:09:54 +0100

    Update library.py
    Update experiments based on new meas_reset flag.

--- surface_sim/experiments/library.py
+++ surface_sim/experiments/library.py
@@ -1,6 +1,6 @@
 from stim import Circuit
 
-from ..circuits import init_qubits, log_meas, qec_round, MEASUREMENT_RESET
+from ..circuits import init_qubits, log_meas, qec_round
 from ..models import Model
 
 
@@ -9,24 +9,25 @@ def memory_exp(
     num_rounds: int,
     log_state: int,
     rot_basis: bool = False,
+    meas_reset: bool = False,
 ) -> Circuit:
 
     init_circ = init_qubits(model, log_state, rot_basis)
-    qec_circ = qec_round(model)
-    meas_circuit = log_meas(model, rot_basis)
+    qec_circ = qec_round(model, meas_reset=meas_reset)
+    meas_circuit = log_meas(model, rot_basis, meas_reset)
 
-    if MEASUREMENT_RESET:
+    if meas_reset:
         experiment = (
             init_circ
-            + qec_round(model, meas_comparison=False)
+            + qec_round(model, meas_comparison=False, meas_reset=meas_reset)
             + qec_circ * (num_rounds - 1)
             + meas_circuit
         )
     else:
         experiment = (
             init_circ
-            + qec_round(model, meas_comparison=False)
-            + qec_round(model, meas_comparison=False)
+            + qec_round(model, meas_comparison=False, meas_reset=meas_reset)
+            + qec_round(model, meas_comparison=False, meas_reset=meas_reset)
             + qec_circ * (num_rounds - 2)
             + meas_circuit
         )
