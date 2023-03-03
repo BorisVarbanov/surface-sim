531ecfab77f5990562fd93dfb21ec295f4d1b9ae
MarcSerraPeralta <marcserraperalta@gmail.com>
2023-03-03 15:10:32 +0100

    Improve function to calculate pauli probabilities

--- surface_sim/models/library.py
+++ surface_sim/models/library.py
@@ -1,5 +1,6 @@
 from typing import Iterable, Iterator, List, Tuple
 import numpy as np
+from itertools import product
 
 from qec_util import Layout
 from stim import CircuitInstruction
@@ -13,6 +14,34 @@ def grouper(iterable: Iterable[str], block_size: int) -> Iterator[Tuple[str, ...
     # grouper('ABCDEFG', 3) --> ABC DEF ValueError
     args = [iter(iterable)] * block_size
     return zip(*args, strict=True)
+
+
+def get_biased_vector(biased_pauli, biased_factor, num_qubits):
+    """
+    biased_factor = r_B / r_N
+
+    r = vector of r_i, with sum_i r_i = 1
+
+    r_i = r_B if i is a biased Pauli
+    r_i = r_N if i is a non-biased Pauli
+    """
+    paulis = ["1", "X", "Y", "Z"]
+    # get all pauli combinations and remove identity operator
+    combinations = list(product(paulis, repeat=num_qubits))[1:]
+    N_B = len([i for i in combinations if biased_pauli in i])
+    N_N = len(combinations) - N_B
+    r_N = 1 / (N_B * biased_factor + N_N)
+    r_B = biased_factor * r_N
+
+    r = []
+    for combination in combinations:
+        if biased_pauli in combination:
+            r.append(r_B)
+        else:
+            r.append(r_N)
+    r = np.array(r)
+
+    return r
 
 
 class CircuitNoiseModel(Model):
@@ -86,56 +115,19 @@ class BiasedCircuitNoiseModel(Model):
     def __init__(self, setup: Setup, layout: Layout) -> None:
         super().__init__(setup, layout)
 
-    def prob1(self, error_operation, qubit):
-        rB = 1 / (1 + 1 / self.param("bias_factor", qubit))
-        rj = (1 - rB) / 2
-        p = self.param(error_operation, qubit)
-        if self.param("bias_pauli", qubit) == 1:  # Pauli X
-            rx, ry, rz = rB, rj, rj
-        elif self.param("bias_pauli", qubit) == 2:  # Pauli Y
-            rx, ry, rz = rj, rB, rj
-        elif self.param("bias_pauli", qubit) == 3:  # Pauli Z
-            rx, ry, rz = rj, rj, rB
-        else:
-            raise TypeError("'bias_pauli' attribute from setup must be: 1, 2 or 3")
-
-        r = np.array([rx, ry, rz])
-        return p * r  # [p * rx, p * ry, p * rz]
-
-    def prob2(self, error_operation, qubit1, qubit2):
-        rB = (1 / 8) * 1 / (1 + 1 / self.param("bias_factor", qubit1, qubit2))
-        rj = (1 - 8 * rB) / 7
-        p = self.param(error_operation, qubit1, qubit2)
-        if self.param("bias_pauli", qubit1, qubit2) == 1:  # Pauli X
-            rix, riy, riz = rB, rj, rj
-            rxi, rxx, rxy, rxz = rB, rB, rB, rB
-            ryi, ryx, ryy, ryz = rj, rB, rj, rj
-            rzi, rzx, rzy, rzz = rj, rB, rj, rj
-        elif self.param("bias_pauli", qubit1, qubit2) == 2:  # Pauli Y
-            rix, riy, riz = rj, rB, rj
-            rxi, rxx, rxy, rxz = rj, rj, rB, rj
-            ryi, ryx, ryy, ryz = rB, rB, rB, rB
-            rzi, rzx, rzy, rzz = rj, rj, rB, rj
-        elif self.param("bias_pauli", qubit1, qubit2) == 3:  # Pauli Z
-            rix, riy, riz = rj, rj, rB
-            rxi, rxx, rxy, rxz = rj, rj, rj, rB
-            ryi, ryx, ryy, ryz = rj, rj, rj, rB
-            rzi, rzx, rzy, rzz = rB, rB, rB, rB
-        else:
-            raise TypeError("'bias_pauli' attribute from setup must be: 1, 2 or 3")
-
-        r = np.array(
-            [rix, riy, riz, rxi, rxx, rxy, rxz, ryi, ryx, ryy, ryz, rzi, rzx, rzy, rzz]
-        )
-        return p * r
-
     def x_gate(self, qubits: List[str]) -> Iterator[CircuitInstruction]:
         inds = self.layout.get_inds(qubits)
         yield CircuitInstruction("X", inds)
 
         for qubit, ind in zip(qubits, inds):
-            probs = self.prob1("sq_error_prob", qubit)
-            yield CircuitInstruction("PAULI_CHANNEL_1", [ind], probs)
+            prob = self.param("sq_error_prob", qubit)
+            r = get_biased_vector(
+                biased_pauli=self.param("biased_pauli", qubit),
+                biased_factor=self.param("biased_factor", qubit),
+                num_qubits=1,
+            )
+            prob = prob * r
+            yield CircuitInstruction("PAULI_CHANNEL_1", [ind], [prob])
 
     def z_gate(self, qubits: List[str]) -> Iterator[CircuitInstruction]:
         inds = self.layout.get_inds(qubits)
@@ -143,7 +135,13 @@ class BiasedCircuitNoiseModel(Model):
         yield CircuitInstruction("Z", inds)
 
         for qubit, ind in zip(qubits, inds):
-            probs = self.prob1("sq_error_prob", qubit)
+            prob = self.param("sq_error_prob", qubit)
+            r = get_biased_vector(
+                biased_pauli=self.param("biased_pauli", qubit),
+                biased_factor=self.param("biased_factor", qubit),
+                num_qubits=1,
+            )
+            prob = prob * r
             yield CircuitInstruction("PAULI_CHANNEL_1", [ind], probs)
 
     def hadamard(self, qubits: List[str]) -> Iterator[CircuitInstruction]:
@@ -152,7 +150,13 @@ class BiasedCircuitNoiseModel(Model):
         yield CircuitInstruction("H", inds)
 
         for qubit, ind in zip(qubits, inds):
-            probs = self.prob1("sq_error_prob", qubit)
+            prob = self.param("sq_error_prob", qubit)
+            r = get_biased_vector(
+                biased_pauli=self.param("biased_pauli", qubit),
+                biased_factor=self.param("biased_factor", qubit),
+                num_qubits=1,
+            )
+            prob = prob * r
             yield CircuitInstruction("PAULI_CHANNEL_1", [ind], probs)
 
     def cphase(self, qubits: List[str]) -> Iterator[CircuitInstruction]:
@@ -164,7 +168,13 @@ class BiasedCircuitNoiseModel(Model):
         yield CircuitInstruction("CZ", inds)
 
         for qubit_pair, ind_pair in zip(grouper(qubits, 2), grouper(inds, 2)):
-            probs = self.prob2("cz_error_prob", *qubit_pair)
+            prob = self.param("cz_error_prob", *qubit_pair)
+            r = get_biased_vector(
+                biased_pauli=self.param("biased_pauli", *qubit_pair),
+                biased_factor=self.param("biased_factor", *qubit_pair),
+                num_qubits=2,
+            )
+            prob = prob * r
             yield CircuitInstruction("PAULI_CHANNEL_2", ind_pair, probs)
 
     def measure(self, qubits: List[str]) -> Iterator[CircuitInstruction]:
@@ -188,5 +198,11 @@ class BiasedCircuitNoiseModel(Model):
         inds = self.layout.get_inds(qubits)
 
         for qubit, ind in zip(qubits, inds):
-            probs = self.prob1("idle_error_prob", qubit)
+            prob = self.param("idle_error_prob", qubit)
+            r = get_biased_vector(
+                biased_pauli=self.param("biased_pauli", qubit),
+                biased_factor=self.param("biased_factor", qubit),
+                num_qubits=1,
+            )
+            prob = prob * r
             yield CircuitInstruction("PAULI_CHANNEL_1", [ind], probs)
