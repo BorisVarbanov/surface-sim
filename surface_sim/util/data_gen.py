6c8d5dc2c5a143f28fcdbc94aada87517a4f0d85
Boris Varbanov <b.m.varbanov@gmail.com>
2023-03-03 14:50:10 +0100

    Small reformatting to data_gen

--- surface_sim/util/data_gen.py
+++ surface_sim/util/data_gen.py
@@ -17,52 +17,32 @@ def sample_experiment(
     data_qubits = layout.get_qubits(role="data")
 
     num_anc = len(anc_qubits)
-    num_data = len(data_qubits)
 
     shots = list(range(1, num_shots + 1))
     qec_rounds = list(range(1, num_rounds + 1))
 
     # generate noisy data
     sampler = experiment.compile_sampler(seed=seed)
-    outcome_vec = sampler.sample(num_shots)
+    outcome_vec = sampler.sample(shots=num_shots).astype(bool)
 
     outcomes = outcome_vec.reshape(num_shots, -1)
     anc_outcomes, data_outcomes = np.split(outcomes, [num_rounds * num_anc], axis=1)
     anc_outcomes = anc_outcomes.reshape(num_shots, num_rounds, num_anc)
 
-    anc_meas = DataArray(
-        data=anc_outcomes.astype(bool),
-        dims=["shot", "qec_round", "anc_qubit"],
-        coords=dict(shot=shots, qec_round=qec_rounds, anc_qubit=anc_qubits),
-    )
-
-    data_meas = DataArray(
-        data=data_outcomes.astype(bool),
-        dims=["shot", "data_qubit"],
-        coords=dict(shot=shots, data_qubit=data_qubits),
-    )
+    anc_meas = DataArray(data=anc_outcomes, dims=["shot", "qec_round", "anc_qubit"])
+    data_meas = DataArray(data=data_outcomes, dims=["shot", "data_qubit"])
 
     # generate ideal data
-    sampler = experiment.without_noise().compile_sampler(seed=seed)
-    outcome_vec = sampler.sample(1)
+    ideal_experimnet = experiment.without_noise()
+    sampler = ideal_experimnet.compile_sampler(seed=seed)
+    outcome_vec = sampler.sample(shots=1).astype(bool)
 
-    outcomes = outcome_vec.reshape(1, -1)
-    ideal_anc_outcomes, ideal_data_outcomes = np.split(
-        outcomes, [num_rounds * num_anc], axis=1
-    )
-    ideal_anc_outcomes = ideal_anc_outcomes.reshape(num_rounds, num_anc)
-    ideal_data_outcomes = ideal_data_outcomes.reshape(num_data)
+    outcomes = np.squeeze(outcome_vec)
+    anc_outcomes, data_outcomes = np.split(outcomes, [num_rounds * num_anc])
+    anc_outcomes = anc_outcomes.reshape(num_rounds, num_anc)
 
-    ideal_anc_meas = DataArray(
-        data=ideal_anc_outcomes.astype(bool),
-        dims=["qec_round", "anc_qubit"],
-        coords=dict(qec_round=qec_rounds, anc_qubit=anc_qubits),
-    )
-    ideal_data_meas = DataArray(
-        data=ideal_data_outcomes.astype(bool),
-        dims=["data_qubit"],
-        coords=dict(data_qubit=data_qubits),
-    )
+    ideal_anc_meas = DataArray(data=anc_outcomes, dims=["qec_round", "anc_qubit"])
+    ideal_data_meas = DataArray(data=data_outcomes, dims=["data_qubit"])
 
     dataset = Dataset(
         data_vars=dict(
@@ -71,15 +51,13 @@ def sample_experiment(
             ideal_data_meas=ideal_data_meas,
             ideal_anc_meas=ideal_anc_meas,
         ),
-        coords=dict(seed=seed),
+        coords=dict(
+            seed=seed,
+            shot=shots,
+            qec_round=qec_rounds,
+            anc_qubit=anc_qubits,
+            data_qubit=data_qubits,
+        ),
     )
 
     return dataset
-
-
-def binary_to_int(x: np.ndarray) -> int:
-    return np.sum(2 ** np.arange(len(x))[::-1] * x)
-
-
-def int_to_binary(x: int, bits: int) -> np.ndarray:
-    return np.array(list(map(int, bin(x)[2:].zfill(bits))))
