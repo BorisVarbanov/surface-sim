e8383e9a0179b157de9240ef91364e5f4d69e6f9
Boris Varbanov <b.m.varbanov@gmail.com>
2023-03-20 17:14:33 +0100

    Add download script

---
+++ scripts/download_dataset.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+username="bmvarbanov"
+dataset="20230317-d3_rot-xzzx-surface_circ-level_p0.001_z-bias-scan"
+from_scrath=false
+
+
+
+if $from_scrath;
+then
+    host="login.delftblue.tudelft.nl"
+    source_dir="/scratch/${username}/data/${dataset}"
+else
+    host="linux-bastion.tudelft.nl"
+    source_dir="/tudelft.net/staff-umbrella/qrennd/data/${dataset}"
+fi
+
+destination_dir="$HOME/data/"
+
+rsync -avzh --exclude "*.ipynb" --progress --stats --no-perms "${username}@${host}:${source_dir}" "${destination_dir}"
