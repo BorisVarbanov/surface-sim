d681ef6945213d2e559bc22e6c03691e1926a355
Boris Varbanov <b.m.varbanov@gmail.com>
2023-07-21 18:17:25 +0200

    Update data_gen.py

--- surface_sim/util/data_gen.py
+++ surface_sim/util/data_gen.py
@@ -19,45 +19,47 @@ def sample_experiment(
     num_anc = len(anc_qubits)
 
     shots = list(range(1, num_shots + 1))
-    qec_rounds = list(range(1, num_rounds + 1))
 
     # generate noisy data
     sampler = experiment.compile_sampler(seed=seed)
     outcome_vec = sampler.sample(shots=num_shots).astype(bool)
 
     outcomes = outcome_vec.reshape(num_shots, -1)
-    anc_outcomes, data_outcomes = np.split(outcomes, [num_rounds * num_anc], axis=1)
-    anc_outcomes = anc_outcomes.reshape(num_shots, num_rounds, num_anc)
 
-    anc_meas = DataArray(data=anc_outcomes, dims=["shot", "qec_round", "anc_qubit"])
-    data_meas = DataArray(data=data_outcomes, dims=["shot", "data_qubit"])
+    if num_rounds:
+        anc_outcomes, data_outcomes = np.split(outcomes, [num_rounds * num_anc], axis=1)
+        anc_outcomes = anc_outcomes.reshape(num_shots, num_rounds, num_anc)
+
+        anc_meas = DataArray(data=anc_outcomes, dims=["shot", "qec_round", "anc_qubit"])
+        data_meas = DataArray(data=data_outcomes, dims=["shot", "data_qubit"])
+    else:
+        data_meas = DataArray(data=outcomes, dims=["shot", "data_qubit"])
 
     # generate ideal data
     ideal_experimnet = experiment.without_noise()
     sampler = ideal_experimnet.compile_sampler(seed=seed)
     outcome_vec = sampler.sample(shots=1).astype(bool)
-
     outcomes = np.squeeze(outcome_vec)
-    anc_outcomes, data_outcomes = np.split(outcomes, [num_rounds * num_anc])
-    anc_outcomes = anc_outcomes.reshape(num_rounds, num_anc)
 
-    ideal_anc_meas = DataArray(data=anc_outcomes, dims=["qec_round", "anc_qubit"])
-    ideal_data_meas = DataArray(data=data_outcomes, dims=["data_qubit"])
+    if num_rounds:
+        anc_outcomes, data_outcomes = np.split(outcomes, [num_rounds * num_anc])
+        anc_outcomes = anc_outcomes.reshape(num_rounds, num_anc)
 
-    dataset = Dataset(
-        data_vars=dict(
-            anc_meas=anc_meas,
-            data_meas=data_meas,
-            ideal_data_meas=ideal_data_meas,
-            ideal_anc_meas=ideal_anc_meas,
-        ),
-        coords=dict(
-            seed=seed,
-            shot=shots,
-            qec_round=qec_rounds,
-            anc_qubit=anc_qubits,
-            data_qubit=data_qubits,
-        ),
-    )
+        ideal_anc_meas = DataArray(data=anc_outcomes, dims=["qec_round", "anc_qubit"])
+        ideal_data_meas = DataArray(data=data_outcomes, dims=["data_qubit"])
+    else:
+        ideal_data_meas = DataArray(data=outcomes, dims=["data_qubit"])
 
+    data_vars = dict(data_meas=data_meas, ideal_data_meas=ideal_data_meas)
+    coords = dict(seed=seed, shot=shots, data_qubit=data_qubits)
+
+    if num_rounds:
+        data_vars["anc_meas"] = anc_meas
+        data_vars["ideal_anc_meas"] = ideal_anc_meas
+
+        qec_rounds = list(range(1, num_rounds + 1))
+        coords["qec_round"] = qec_rounds
+        coords["anc_qubit"] = anc_qubits
+
+    dataset = Dataset(data_vars=data_vars, coords=coords)
     return dataset
